3c9d8337c53fdb0f901baed70f37bf6082fc690d
Leonid-Korobkov <korobkov-leo@mail.ru>
2024-12-01 22:12:56 +0300

    change config start

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const logger = require('morgan')
 const fs = require('fs')
 
 const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 app.use(logger('dev'))
 app.use(express.json())
