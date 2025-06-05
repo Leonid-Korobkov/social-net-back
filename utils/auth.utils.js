720ad3547d88a4ebf41b738c30aec97b0122010a
Leonid-Korobkov <korobkov-leo@mail.ru>
2025-06-05 19:54:20 +0300

    fix: validatePassword character

--- utils/auth.utils.js
+++ utils/auth.utils.js
@@ -33,7 +33,7 @@ const validatePassword = password => {
   const hasUpperCase = /[A-Z]/.test(password)
   const hasLowerCase = /[a-z]/.test(password)
   const hasNumbers = /\d/.test(password)
-  const hasSpecialChar = /[!@#$%^&*-(),.?":{}|<>]/.test(password)
+  const hasSpecialChar = /[-!@#$%^&*(),.?":{}|<>]/.test(password)
 
   const errors = []
   if (password.length < minLength) {
